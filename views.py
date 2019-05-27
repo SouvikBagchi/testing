a42ec0efb178d2910e70e68401c3c586cca6e4d9
souvikbagchi <souvik.bagchi@gmail.com>
2019-05-27 13:12:00 -0500

    testing sessions

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import pandas as pd
+import random
 
 app = Flask(__name__)
 app.secret_key = 'verysecretsecretkey'
@@ -31,27 +32,59 @@ def recommend_joke():
         #we will randomly select one of the highest rated joke and display it
         #insert code to get the joke
 
-        session['joke_num'] = 0 #this is default but you will need to get the joke number you are displaying
-        return render_template('joke.html', joke= joke)
+        session['joke_num'] = random.randint(1,90) #this is default but you will need to get the joke number you are displaying
+        return render_template('joke.html', joke= joke+str(user_pref))
     
     else:
 
-        #
+        #This is the value which the user gave to the previous joke which is represented by joke_num
+        value = request.form["rating"]
+        #get the joke_num
+        joke_num = session['joke_num']
+        session.pop('joke_num',None)
+        
+        #now that we have the joke number we will create the svd with this information
+        
+        #fetch the data
+        #create the matrix
+        #append the user_pref
+        #calculate svd
+        
+        
         
         #check if session is set
         if 'user_pref' in session:
 
-            pass
+            curr_user_pref = session['user_pref']
+            session.pop('user_pref',None)
+            last_joke = session['joke_num']
+            session.pop('joke_num',None)
+            
+            
+            print('curr user pref ',curr_user_pref)
+            return render_template('recommended_jokes.html', joke= "recommended joke"+str(curr_user_pref))
 
         else :
-
-            value = request.form["rating"]
-
-            #create a list
+            
+            
+            #for that we will set a user_pref
+            user_pref = []*100 #create a list with the lenght of the number of jokes
+            user_pref[joke_num] = value
+            
+            #fetch the data
+            #create the matrix
+            #append the user_pref
             #calculate svd
-
+            
+            new_joke = 'new joke'
+            new_joke_number = random.randint(1,90)#needs to be the one that is being recommend_joke
+            
+            #set the sessions
+            session['joke_num'] = new_joke_number
+            session['user_pref'] = user_pref #[0,0,0,0,0,0,0,5,00,0,0,0,]
+            
             #store in session
-            return render_template('recommended_jokes.html', joke= "recommended joke")
+            return render_template('recommended_jokes.html', joke= "recommended joke"+str(user_pref))
     
 
 if __name__ == "__main__":
