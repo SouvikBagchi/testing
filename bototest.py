0a693f05e3eba490d7d76857fcc9aaa5e1a6178c
root <root@ip-172-31-44-85.us-east-2.compute.internal>
2019-06-01 17:53:22 +0000

    change file path name

--- bototest.py
+++ bototest.py
@@ -3,4 +3,4 @@ s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
 bucket = s3_resource.Bucket('jokerecommender')
-bucket.download_file('/ratings.csv','ratingtest.csv')
+bucket.download_file('ratings.csv','ratingtest.csv')
