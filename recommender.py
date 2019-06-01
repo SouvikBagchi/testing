fbf2414ec945be656e3d610e17463d28661ac7c7
anjaliverma96 <anjaliverma2020@u.northwestern.edu>
2019-06-01 15:26:25 -0500

    changes;

--- recommender.py
+++ recommender.py
@@ -18,14 +18,16 @@ class Recommender:
 		self.data_jokes = data_joke
 
 	def get_most_popular(self):
-	    popular_rated = self.data_final[self.data_final['Rating'] == 10]
-	    popular_jokes = popular_rated.groupby('JokeID').count().reset_index()
-	    popular_jokes = popular_jokes[['JokeID','Rating']]
-	    popular_jokes.columns = ['JokeID','Number_rated10']
-	    top_joke = popular_jokes.sort_values(by=['Number_rated10']).head(1)
-	    top_joke_val = top_joke['JokeID'].values[0]
-	    jokes_list = self.data_final['JokeID'].unique().tolist()
-    	joke_num = jokes_list.index(top_joke_val)
-	    top_joke_desc = self.data_jokes[self.data_jokes['JokeID'] == top_joke_val].values[0][1]
+		popular_rated = self.data_final[self.data_final['Rating'] == 10]
+		popular_jokes = popular_rated.groupby('JokeID').count().reset_index()
+		popular_jokes = popular_jokes[['JokeID','Rating']]
+		popular_jokes.columns = ['JokeID','Number_rated10']
+		top_joke = popular_jokes.sort_values(by=['Number_rated10']).head(1)
+		top_joke_val = top_joke['JokeID'].values[0]
+		jokes_list = self.data_final['JokeID'].unique().tolist()
+		joke_num = jokes_list.index(top_joke_val)
+		top_joke_desc = self.data_jokes[self.data_jokes['JokeID'] == top_joke_val].values[0][1]
 
-	    return top_joke_desc, joke_num
+		return top_joke_desc, joke_num
+
+
