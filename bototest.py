5a9bdf2d7b751b2ee0ed71bf230b952a337b79dc
souvikbagchi <souvik.bagchi@gmail.com>
2019-06-01 13:08:45 -0500

    final boto class test2

--- bototest.py
+++ bototest.py
@@ -3,14 +3,16 @@ import boto3
 
 class Boto:
 
-	def download_rating():
+	def download_rating(self):
 
 		s3_resource = boto3.resource('s3')
 		bucket = s3_resource.Bucket('jokerecommender')
 		bucket.download_file('ratings.csv','rating.csv')
 
-	def download_jokes():
+	def download_jokes(self):
 		
 		s3_resource = boto3.resource('s3')
 		bucket = s3_resource.Bucket('jokerecommender')
 		bucket.download_file('jokes.csv','jokes.csv')
+
+		
