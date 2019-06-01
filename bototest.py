279ede201e031f974babbc88a2c52d446d56d159
souvikbagchi <souvik.bagchi@gmail.com>
2019-06-01 12:36:42 -0500

    bototest 2

--- bototest.py
+++ bototest.py
@@ -3,4 +3,5 @@ s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
 s3_resource.Object('jokerecommender', 'jokes.csv').download_file(
-    f'/{'jokes.csv'}')
+    f'/{jokes.csv}')
+
